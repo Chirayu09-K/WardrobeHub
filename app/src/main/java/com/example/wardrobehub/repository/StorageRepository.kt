65027ce67e3db44eab46b9c2d3ab7ba48b04deb6
Chirayu09-K <chirayukhatri10@gmail.com>
2025-12-18 19:52:39 +0545

    Removed unnecessary/unused files and folders

--- app/src/main/java/com/example/wardrobehub/repository/StorageRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.wardrobehub.repository
-
-import android.net.Uri
-import kotlinx.coroutines.flow.Flow
-
-interface StorageRepository {
-    fun uploadImage(userId: String, uri: Uri): Flow<Result<String>>
-}
